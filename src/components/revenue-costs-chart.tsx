6c009ae32a349d1209f276542547a8d02ee93d51
Acassius Alves <acassiusalves@gmail.com>
2025-09-06 19:05:23 +0000

    O grafico Receita, Custos e Descontos pode fazer nesse formato

--- src/components/revenue-costs-chart.tsx
+++ src/components/revenue-costs-chart.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import * as React from "react";
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from "recharts";
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from "recharts";
 import {
   ChartConfig,
   ChartContainer,
@@ -42,10 +42,11 @@ export default function RevenueCostsChart({ data }: RevenueCostsChartProps) {
   return (
     <ChartContainer config={chartConfig} className="min-h-[300px] w-full">
       <ResponsiveContainer width="100%" height={350}>
-        <BarChart
+        <AreaChart
           data={data}
-          margin={{ top: 20, right: 20, bottom: 0, left: -20 }}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis
             dataKey="month"
             stroke="#888888"
@@ -78,11 +79,11 @@ export default function RevenueCostsChart({ data }: RevenueCostsChartProps) {
              />}
           />
           <Legend />
-          <Bar dataKey="revenue" name="Faturamento" stackId="a" fill="var(--color-revenue)" radius={[4, 4, 0, 0]}/>
-          <Bar dataKey="discounts" name="Descontos" stackId="a" fill="var(--color-discounts)" />
-          <Bar dataKey="cmv" name="CMV" stackId="a" fill="var(--color-cmv)" />
-          <Bar dataKey="shipping" name="Frete" stackId="a" fill="var(--color-shipping)" />
-        </BarChart>
+          <Area type="monotone" dataKey="revenue" stackId="1" stroke="var(--color-revenue)" fill="var(--color-revenue)" fillOpacity={0.4} name="Faturamento"/>
+          <Area type="monotone" dataKey="discounts" stackId="1" stroke="var(--color-discounts)" fill="var(--color-discounts)" fillOpacity={0.4} name="Descontos"/>
+          <Area type="monotone" dataKey="cmv" stackId="1" stroke="var(--color-cmv)" fill="var(--color-cmv)" fillOpacity={0.4} name="CMV"/>
+          <Area type="monotone" dataKey="shipping" stackId="1" stroke="var(--color-shipping)" fill="var(--color-shipping)" fillOpacity={0.4} name="Frete"/>
+        </AreaChart>
       </ResponsiveContainer>
     </ChartContainer>
   );
